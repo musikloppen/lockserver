857261c42f7c82f431c1ea92548d39aca2e0f2ec
Kristoffer Ek <stoffer@skulp.net>
2026-07-22 08:39:10 +0200

    mprove database connection error handling in LockServer::Db

--- perl/LockServer/Db.pm
+++ perl/LockServer/Db.pm
@@ -3,16 +3,15 @@ package LockServer::Db;
 use strict;
 use warnings;
 use DBI;
-use Sys::Syslog;
 
 my $dsn = "DBI:mysql:;mysql_read_default_file=/etc/mysql/conf.d/99-client.cnf";
 
 sub my_connect {
-	my $dbh = DBI->connect($dsn, undef, undef, { 
+	my $dbh = DBI->connect($dsn, undef, undef, {
 		mysql_auto_reconnect => 1,
 		RaiseError => 0,
 		PrintError => 0
-	}) or warn $!;
+	}) or die "DB Error: " . ($DBI::errstr || $!) . "\n";
 	
 	return $dbh;
 }
